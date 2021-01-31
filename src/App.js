bc213b34d0e46bc11901bdb59fe0ef93e6523ddc
Lawrence O. Caulker <locaulker@gmail.com>
2021-01-31 10:00:16 -0500

    setup array of arrays

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <main>
       <div className='section-title'>
-        <h1>{loading ? 'loading' : 'pagination'}</h1>
+        <h1>{loading ? 'loading...' : 'pagination'}</h1>
         <div className='underline'></div>
       </div>
       <section className='followers'>
